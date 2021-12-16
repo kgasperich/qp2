1e5a9c20f9e998325cb483115fd023ba03633148
Kevin Gasperich <kgasperich@gmail.com>
2021-12-16 14:26:59 -0600

    bitmask utils for ionized core

--- src/bitmask/find_hole.irp.f
+++ src/bitmask/find_hole.irp.f
@@ -53,3 +53,100 @@ logical function is_the_particl_in_det(key_in,ispin,i_particl)
  endif
 
 end
+BEGIN_PROVIDER [integer, dim_list_ionized_core_orb]
+  implicit none
+  BEGIN_DOC
+  ! dimensions for the allocation of list_core.
+  ! it is at least 1
+  END_DOC
+   dim_list_ionized_core_orb = max(n_ionized_core_orb,1)
+END_PROVIDER
+
+
+ BEGIN_PROVIDER [ integer, list_ionized_core        , (dim_list_ionized_core_orb) ]
+&BEGIN_PROVIDER [ integer, list_ionized_core_reverse, (mo_num) ]
+  implicit none
+  BEGIN_DOC
+  ! List of MO indices which are in the core.
+  END_DOC
+  integer                        :: i, n
+  list_ionized_core = 0
+  list_ionized_core_reverse = 0
+
+  n=0
+  do i = 1, mo_num
+    !if(mo_class(i) == 'Core')then
+    if(i <= n_ionized_core_orb)then
+      n += 1
+      list_ionized_core(n) = i
+      list_ionized_core_reverse(i) = n
+    endif
+  enddo
+  print *,  'Ionized Core MOs:'
+  print *,  list_ionized_core(1:n_ionized_core_orb)
+
+END_PROVIDER
+
+
+ BEGIN_PROVIDER [ integer(bit_kind), ionized_core_bitmask , (N_int,2) ]
+&BEGIN_PROVIDER [ integer, n_int_ionized_core_max ]
+  implicit none
+  BEGIN_DOC
+  ! Bitmask identifying the ionized core MOs
+  END_DOC
+  integer :: i,ispin
+  ionized_core_bitmask  = 0_bit_kind
+  if(n_ionized_core_orbs > 0)then
+    call list_to_bitstring( ionized_core_bitmask(1,1), list_ionized_core, n_ionized_core_orb, N_int)
+    call list_to_bitstring( ionized_core_bitmask(1,2), list_ionized_core, n_ionized_core_orb, N_int)
+  endif
+  n_int_ionized_core_max=0
+  do i=1,N_int
+    do ispin=1,2
+      if (popcnt(ionized_core_bitmask(i,ispin))>0) then
+        n_int_ionized_core_max=i
+      endif
+    enddo
+  enddo
+
+ END_PROVIDER
+
+
+subroutine ab_holes_in_ionized_core(key_in,ab_holes)
+  use bitmasks
+  ! returns holes of each spin in ionized core orbs of key_in
+  implicit none
+  integer(bit_kind), intent(in) :: key_in(N_int,2)
+  integer, intent(out) :: ab_holes(2)
+  integer(bit_kind) :: key_tmp
+  integer :: i,ispin
+  ab_holes = 0
+  do i = 1, n_int_ionized_core_max
+    do ispin = 1,2
+      key_tmp = iand(ionized_core_bitmask(i,ispin),not(key_in(i,ispin)))
+      ab_holes(ispin) += popcnt(key_tmp)
+    enddo
+  enddo
+end
+
+integer function n_holes_in_ionized_core(key_in)
+  use bitmasks
+  ! returns holes of each spin in ionized core orbs of key_in
+  implicit none
+  integer(bit_kind), intent(in) :: key_in(N_int,2)
+  integer :: i,ispin
+  n_holes_in_ionized_core = 0
+  do i = 1, n_int_ionized_core_max
+    do ispin = 1,2
+      n_holes_in_ionized_core += popcnt(iand(ionized_core_bitmask(i,ispin),not(key_in(i,ispin))))
+    enddo
+  enddo
+end
+
+logical function det_allowed_ionized_core(key_in)
+  use bitmasks
+  ! returns .True. if key_in has the proper number of holes in the core
+  implicit none
+  integer(bit_kind), intent(in) :: key_in(N_int,2)
+  det_allowed_ionized_core = (n_holes_in_ionized_core(key_in) == n_core_holes)
+end
